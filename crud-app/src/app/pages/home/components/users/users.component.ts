2fee222e4baf360faa938556c07bd03fbcb781d5
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2024-05-19 00:31:01 -0300

    Edit usrs

--- crud-app/src/app/pages/home/components/users/users.component.ts
+++ crud-app/src/app/pages/home/components/users/users.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  done: boolean;
+  description: string;
+}
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  taskList: Task[] = []
 
+  constructor() {
+    this.taskList = [
+      {done: false, description: "Clean the house"},
+      {done: false, description: "Wash the car"},
+      {done: true, description: "Feed the dog"},
+    ]
+  }
+
+  changeTask(task: Task) {
+    task.done = !task.done
+  }
 }
