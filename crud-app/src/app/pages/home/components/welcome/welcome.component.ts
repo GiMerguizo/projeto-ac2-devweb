1b739086473d4a7fd100fdee34926ab808feed08
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2024-05-19 00:39:27 -0300

    Edited Home

--- crud-app/src/app/pages/home/components/welcome/welcome.component.ts
+++ crud-app/src/app/pages/home/components/welcome/welcome.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  done: boolean;
+  description: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+
+  taskList: Task[] = []
+
+  constructor() {
+    this.taskList = [
+      {done: false, description: "Clean the house"},
+      {done: false, description: "Wash the car"},
+      {done: true, description: "Feed the dog"},
+    ]
+  }
+
+  changeTask(task: Task) {
+    task.done = !task.done
+  }
 
 }
