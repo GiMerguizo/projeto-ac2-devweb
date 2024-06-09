1ee470aeb7ada62cb77153396061bbb74777be70
Giovana Merguizo <giovana.merguizo@facens.br>
2024-06-09 16:00:06 -0300

    Modified files

--- Backend/controllers/usersController.js
+++ Backend/controllers/usersController.js
@@ -1,7 +1,7 @@
 const { randomUUID } = require('crypto')
 const express = require('express');
 const router = express.Router();
-// const User = require('.././models/Users')
+const User = require('.././models/Users')
 
 var users = []
 
