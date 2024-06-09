0c0bc444bbae851c66f0ce1bcddef547c75ca5ad
Giovana Merguizo <giovana.merguizo@facens.br>
2024-06-09 15:11:16 -0300

    Funcionalidades

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,79 @@
-const http = require('http');
+const express = require('express')
+const { randomUUID } = require('crypto')
 
-const hostname = 'localhost';
-const port = 3000;
+const porta = 8080
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    
-    if (req.url == '/produto') {
-        res.end(JSON.stringify({mensagem: "url de produto"}));
+
+const app = express()
+app.use(express.json())
+
+var users = []
+
+app.get("/users", (req, res) => {
+    return res.json(users)
+})
+
+app.post("/users", (req, res) => {
+
+    var { nome, email } = req.body;
+
+    var user = {
+        id: randomUUID(),
+        nome: nome, 
+        email: email
     }
-    else
-        res.end(JSON.stringify({mensagem : "teste"}));
-});
 
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
-});
+    users.push(user)
+
+    console.log("Usuario adicionado com sucesso!")
+    return res.json(
+        {
+            mensagem: "Usuario adicionado com sucesso!",
+            user: user
+        }
+    )
+})
+
+app.put("/users/:id", (req, res) => {
+    var id = req.params.id
+    var { nome, email } = req.body;
+
+
+    users.map( user => {
+        if(user.id == id) {
+            user.nome = nome,
+            user.email = email
+        }
+    } )
+
+    console.log("Usuario editado com sucesso!")
+
+    return res.json(
+        {
+            mensagem: "Usuario adicionado com sucesso!",
+            users: users
+        }
+    )
+})
+
+app.delete("/users/:id", (req, res) => {
+    var id = req.params.id
+
+    users = users.filter( user => {
+        return user.id != id
+    })
+
+    console.log("Usuario deletado com sucesso!")
+
+    return res.json(
+        {
+            mensagem: "Usuario deletado com sucesso!",
+            users: users
+        }
+    )
+})
+
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`)
+})
